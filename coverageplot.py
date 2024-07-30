a5f0393e5b54855e3ad9cc6cc4e36ac2f7973ad3
philip-hub <philipstudentemail@gmail.com>
2024-07-30 13:32:50 -0400

    added more information for debuging

--- coverageplot.py
+++ coverageplot.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np 
 
 csv_file_path = "coverage_with_x_and_median.csv"
 data = pd.read_csv(csv_file_path)
 
 if 'x' in data.columns and 'y' in data.columns and 'arm' in data.columns:
     plt.figure(figsize=(20, 3))
-    plt.scatter(data['x'], data['y'], s=10, alpha=0.5, c='lightgreen')  # s parameter controls the size of the dots
+    plt.scatter(data['x'], np.log2(data['y']), s=10, alpha=0.5, c='lightgreen')  # s parameter controls the size of the dots
     plt.title('Coverage Plot')
     plt.xlabel('position')
     plt.ylabel('log2(median/ref)')
