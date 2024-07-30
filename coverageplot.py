8d09884910e4f321b0262c7fd99597139de7ae3c
philip-hub <philipstudentemail@gmail.com>
2024-07-30 11:25:13 -0400

    updated labels

--- coverageplot.py
+++ coverageplot.py
@@ -1,25 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# File path to the CSV file
 csv_file_path = "coverage_with_x_and_median.csv"
-
-# Read the CSV file into a DataFrame
 data = pd.read_csv(csv_file_path)
 
-# Check if the necessary columns exist
 if 'x' in data.columns and 'y' in data.columns and 'arm' in data.columns:
-    # Create a scatter plot with smaller dots
-    plt.figure(figsize=(20, 4))
-    plt.scatter(data['x'], data['y'], s=10, alpha=0.5)  # 's' parameter controls the size of the dots
-    
-    # Add title and labels
+    plt.figure(figsize=(20, 3))
+    plt.scatter(data['x'], data['y'], s=10, alpha=0.5, c='lightgreen')  # s parameter controls the size of the dots
     plt.title('Coverage Plot')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel('position')
+    plt.ylabel('log2(median/ref)')
     plt.grid(True)
 
-    # Add arm labels as tickers on the x-axis
+
     x_ticks = []
     x_labels = []
     previous_arm = None
@@ -30,7 +23,7 @@ if 'x' in data.columns and 'y' in data.columns and 'arm' in data.columns:
             x_labels.append(arm)
             previous_arm = arm
     
-    plt.xticks(ticks=x_ticks, labels=x_labels, rotation=90)  # Rotate labels for better visibility
+    plt.xticks(ticks=x_ticks, labels=x_labels, rotation=90)
     
     # Show the plot
     plt.tight_layout()
