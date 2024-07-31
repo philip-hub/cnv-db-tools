c0b917cfb8eb1b7572f398a22184cbc77e249ca7
philip-hub <philipstudentemail@gmail.com>
2024-07-31 15:43:31 -0400

    plan for master.py

--- master.py
+++ master.py
@@ -1,1 +1,25 @@
-#this file will make the final dataframe for the interactive app
+"""
+This is the file that exports the final df for the interactive web app
+
+### Coverage will export: X1, Y1, ARM1, M, & R
+
+user options: filter and base log
+
+ advanced options: change the column names of relevant columns
+
+
+### vaf will export: X2, Y2, ARM2
+
+user options: filter
+
+ advanced options: change the column names of relevant columns
+
+
+### CN_AI will export: X23, Y3, ARM3
+
+user options: filter
+
+advanced options: change the column names of relevant columns
+
+
+"""
