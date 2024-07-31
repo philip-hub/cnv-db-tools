103028bb63aafd2a1ec4090ae1d7904d50c80984
philip-hub <philipstudentemail@gmail.com>
2024-07-31 12:39:53 -0400

    got rid of some easy comments

--- plot_CN_AI.py
+++ plot_CN_AI.py
@@ -4,11 +4,9 @@ import pandas as pd
 # Load the CSV file
 df = pd.read_csv('karotype_graph.csv')
 
-# Create the scatter plot
 plt.figure(figsize=(6, 6))
 plt.scatter(df['X2'], df['Y2'], c='blue', alpha=0.5)
 
-# Add titles and labels
 plt.title('Scatter Plot of AI vs CN')
 plt.xlabel('CN')
 plt.ylabel('AI')
@@ -17,5 +15,4 @@ plt.ylabel('AI')
 # plt.xlim(1, 3)
 # plt.ylim(0, 0.5)
 
-# Show the plot
 plt.show()
