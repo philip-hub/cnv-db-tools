28fb65f1026ee5ff73130ef7362f21be55e25f9e
philip-hub <philipstudentemail@gmail.com>
2024-08-01 13:07:22 -0400

    working version next I will make a version to handle multiple files

--- master.py
+++ master.py
@@ -198,3 +198,6 @@ print(cn_ai_df)
 final_df = pd.concat([coverage_df, vaf_df, cn_ai_df], axis=1)
 
 print(final_df)
+
+csv_file_path = "master/master.tsv"
+final_df.to_csv(csv_file_path, index=False)
