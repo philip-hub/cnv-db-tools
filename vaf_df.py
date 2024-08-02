01e949b71b6625718c8657ec4a57d4f09616f95f
philip-hub <philipstudentemail@gmail.com>
2024-08-02 13:54:34 -0400

    ref function no longer needed

--- vaf_df.py
+++ vaf_df.py
@@ -3,12 +3,6 @@ import numpy as np
 import math
 import re
 
-# Function to get the value for ref
-def getRef(kars, ref):
-    dip_list = [ref[i] for i in range(len(kars)) if kars[i] == 'DIP']
-    med_index = math.floor(len(dip_list) / 2)
-    return dip_list[med_index]
-
 #get dataframes
 kar_file_path = "inputs/Data_D1_karyotype.tsv"
 kar_data = pd.read_csv(kar_file_path, sep="\t")
