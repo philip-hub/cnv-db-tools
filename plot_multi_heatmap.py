e0a2c2cb9fc83ec9edb1b3e682700deaf703a5d6
philip-hub <philipstudentemail@gmail.com>
2024-08-08 12:03:15 -0400

    plot the heatmaps

---
+++ plot_multi_heatmap.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+filepath = "multi-output/multi.tsv"
+
+df = pd.read_csv(filepath, sep="\t")
+
+def create_heatmap(data, value_column, title):
+    pivot_df = data.pivot_table(index="sample", columns="arm", values=value_column, aggfunc='mean')
+    
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(pivot_df, cmap="coolwarm", annot=True, fmt=".1f", linewidths=.5, cbar_kws={'label': value_column})
+    plt.title(title)
+    plt.xlabel('Arm')
+    plt.ylabel('Sample Number')
+    plt.show()
+
+create_heatmap(df, 'cn', 'CN Heatmap')
+
+create_heatmap(df, 'ai', 'AI Heatmap')
