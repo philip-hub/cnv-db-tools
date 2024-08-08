fe34c86758c691eaf53fb4fa48ef645c6637e9e1
philip-hub <philipstudentemail@gmail.com>
2024-08-08 11:10:38 -0400

    basic functionality

--- master_multi.py
+++ master_multi.py
@@ -6,3 +6,26 @@ arm, cn, ai, sample
 """
 #imports
 import pandas as pd; import numpy as np; import re
+import os
+
+
+folder = "karfiles/"
+
+sample_number = 0
+
+def getAICN(kar_data):
+    ai = kar_data['ai']
+    dcn = kar_data['cn']  
+    arms_kar_data = kar_data['arm']
+    
+    ai_cn_data = pd.DataFrame({
+        'arm':arms_kar_data,
+        'cn':dcn,
+        'ai':ai
+    })
+    
+    
+    return ai_cn_data
+
+
+
