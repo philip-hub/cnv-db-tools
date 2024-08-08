0fcf2c0ffe8433c1c4dea4ab3d63f0c039084d49
philip-hub <philipstudentemail@gmail.com>
2024-08-08 11:18:19 -0400

    multi file should work now

--- master_multi.py
+++ master_multi.py
@@ -12,6 +12,7 @@ import os
 folder = "karfiles/"
 
 sample_number = 0
+sample_num_in_file_name = True
 
 def getAICN(kar_data):
     ai = kar_data['ai']
@@ -26,6 +27,26 @@ def getAICN(kar_data):
     
     
     return ai_cn_data
+
+
+i = 0
+
+for file in os.listdir(folder):
+    if file.endswith(".tsv"):
+        filepath = os.path(folder, file)
+        
+        kar_data = pd.read_csv(filepath, sep="\t")
+        ai_cn_data = getAICN(kar_data)
+        
+        if sample_num_in_file_name:
+            sample_number = int(re.search(r'\d+', file).group())  # Assuming filenames have numbers
+        else: 
+            sample_number = i
+        ai_cn_data['sample'] = sample_number
+        combined_df = pd.concat([combined_df, ai_cn_data], ignore_index=True)
+        i=+1 #one time spent 2 hours debugging my algorithms hw bc I messed this up
+
+print(combined_df)
 
 
 
