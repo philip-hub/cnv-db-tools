05b69e3d3857e7d666c8e60215bbb4d092b718fe
philip-hub <philipstudentemail@gmail.com>
2024-08-08 12:08:21 -0400

    fixed the colors

--- plot_multi_heatmap.py
+++ plot_multi_heatmap.py
@@ -6,16 +6,16 @@ filepath = "multi-output/multi.tsv"
 
 df = pd.read_csv(filepath, sep="\t")
 
-def create_heatmap(data, value_column, title):
+def create_heatmap(data, value_column, title, color):
     pivot_df = data.pivot_table(index="sample", columns="arm", values=value_column, aggfunc='mean')
     
     plt.figure(figsize=(10, 8))
-    ax = sns.heatmap(pivot_df, cmap="coolwarm", annot=False, linewidths=.5, cbar_kws={'label': value_column})
+    ax = sns.heatmap(pivot_df, cmap=color, annot=False, linewidths=.5, cbar_kws={'label': value_column})
     plt.title(title)
     plt.xlabel('Arm')
     plt.ylabel('Sample Number')
     plt.show()
 
-create_heatmap(df, 'cn', 'CN Heatmap')
+create_heatmap(df, 'cn', 'CN Heatmap','coolwarm')
 
-create_heatmap(df, 'ai', 'AI Heatmap')
+create_heatmap(df, 'ai', 'AI Heatmap','plasma')
