2987921fb1e624d3306e47aa5e8fe142e91f8e8e
philip-hub <philipstudentemail@gmail.com>
2024-08-08 11:38:48 -0400

    exporting multi now

--- master_multi.py
+++ master_multi.py
@@ -10,6 +10,8 @@ import os
 
 
 folder = "karyotypes/"
+output_folder = "multi-output/"
+output_file = "multi.tsv"
 
 sample_number = 0
 sample_num_in_file_name = False
@@ -47,3 +49,8 @@ for file in os.listdir(folder):
         i=i+1 #one time spent 2 hours debugging my algorithms hw bc I messed this up
 
 print(combined_df)
+
+
+os.makedirs(output_folder, exist_ok=True)
+output_path = os.path.join(output_folder, output_file)
+combined_df.to_csv(output_path, sep='\t', index=False)
